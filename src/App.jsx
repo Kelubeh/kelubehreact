b96fb06f2dcbfbb6ab2327d94ce47be2f0bc929a
Kelubeh <kelubeh@gmail.com>
2025-07-06 07:24:46 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Welcome to Kelubeh</h1>
+      <p>This is a simple React application.</p>
+      <p>Enjoy building your app!</p>
+    </div>
   )
 }
 
